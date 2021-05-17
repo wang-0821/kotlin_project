e1862ae1cc8812459bffc992ea3424719f7dd8a6
wang-0821 <wanglx0821@gmail.com>
2021-05-17 15:21:36 +0800

    fix junit properties

--- kotlin-base/src/test/kotlin/ClassLoaderTest.kt
+++ kotlin-base/src/test/kotlin/ClassLoaderTest.kt
@@ -1,5 +1,4 @@
 import com.xiao.base.testing.KtTestBase
-import com.xiao.model.ClassTarget
 import com.xiao.model.CustomClassLoader
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.BeforeAll
@@ -20,9 +19,7 @@ class ClassLoaderTest : KtTestBase() {
     @Test
     fun `test use classLoader loadClass and class forName`() {
         val ktClass2 = classLoader.loadClass(CLASSNAME)
-        println("ClassLoader loadClass done.")
         val ktClass1 = Class.forName(CLASSNAME, true, classLoader)
-        println("Class forName done. ${ClassTarget.val1}")
         Assertions.assertSame(ktClass1, ktClass2)
     }
 
@@ -30,9 +27,7 @@ class ClassLoaderTest : KtTestBase() {
     fun `test use custom classLoader breaking parents delegation`() {
         val customClassLoader = CustomClassLoader()
         val ktClass1 = customClassLoader.loadClass(CLASSNAME)
-        println("ClassLoader loadClass done.")
         val ktClass2 = Class.forName(CLASSNAME, true, customClassLoader)
-        println("Class forName done. ${ClassTarget.val1}")
         Assertions.assertSame(ktClass1, ktClass2)
     }
 
