e013190dd2a3f954170dd189a3d17e599455d70d
wang-0821 <53598654+wang-0821@users.noreply.github.com>
2021-05-19 20:22:24 +0800

    add reference test (#40)

---
+++ kotlin-base/src/test/kotlin/ReferenceTest.kt
@@ -0,0 +1,32 @@
+import com.xiao.base.testing.KtTestBase
+import com.xiao.model.ClassTarget
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+import java.lang.ref.WeakReference
+import java.util.concurrent.atomic.AtomicInteger
+
+/**
+ *
+ * @author lix wang
+ */
+class ReferenceTest : KtTestBase() {
+    @Test
+    fun `test weak reference object clean up by gc`() {
+        var classTarget: ClassTarget? = ClassTarget()
+        val classTargetReference = WeakReference(classTarget)
+        System.gc()
+        Assertions.assertEquals(classTargetReference.get()!!.val2, 2)
+        classTarget = null
+        System.gc()
+        Assertions.assertEquals(classTargetReference.get(), null)
+    }
+
+    @Test
+    fun `test strong reference and weak reference wrapped object`() {
+        val atomicInt = AtomicInteger(1)
+        val weakReferenceInt = WeakReference(atomicInt)
+        Assertions.assertEquals(weakReferenceInt.get()!!.get(), 1)
+        System.gc()
+        Assertions.assertEquals(weakReferenceInt.get()!!.get(), 1)
+    }
+}
