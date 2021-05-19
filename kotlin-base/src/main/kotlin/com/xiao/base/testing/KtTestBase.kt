7cb4888c33bb6499fa6a6c9e455edc4913534531
wang-0821 <53598654+wang-0821@users.noreply.github.com>
2021-05-19 13:08:12 +0800

    improve junit test (#39)
    * improve junit test
    
    * use junit timeout annotation

--- kotlin-base/src/main/kotlin/com/xiao/base/testing/KtTestBase.kt
+++ kotlin-base/src/main/kotlin/com/xiao/base/testing/KtTestBase.kt
@@ -1,10 +1,13 @@
 package com.xiao.base.testing
 
 import org.junit.jupiter.api.TestInstance
+import org.junit.jupiter.api.Timeout
+import java.util.concurrent.TimeUnit
 
 /**
  *
  * @author lix wang
  */
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@Timeout(value = 5, unit = TimeUnit.MINUTES)
 open class KtTestBase
